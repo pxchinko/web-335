aa73dc9e1cc056a4f4e6752a41324e80df71661e
Sarah Massie <smassie@my365.bellevue.edu>
2020-12-03 14:35:56 -0800

    master

---
+++ week-8/massie-calculator.py
@@ -0,0 +1,24 @@
+# ============================================
+# ; Title: massie-calculator.py
+# ; Author: Sarah Massie
+# ; Date: December 3 2020
+# ; Modified By: Sarah Massie
+# ; Description: Making a Python calculator
+# ;===========================================
+first_name = 'Sarah'
+last_name = 'Massie'
+print(first_name + ' ' + last_name)
+
+# print statements
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def divide(x, y):
+    return x/y
+
+print(add(1,2))
+print(subtract(4,1))
+print(divide(8,2))
--- week-8/massie_hello_world.py
+++ week-8/massie_hello_world.py
@@ -1,1 +1,8 @@
+# ============================================
+# ; Title: massie-hello-world.py
+# ; Author: Sarah Massie
+# ; Date: December 3 2020
+# ; Modified By: Sarah Massie
+# ; Description: Testing python
+# ;===========================================
 print('Hello world!')
